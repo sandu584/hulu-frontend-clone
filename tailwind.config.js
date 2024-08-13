783d0efb752cbb6585f278afaba6c3e016d6fee3
Sandeep Raveendra Kolli <135503353+sandu584@users.noreply.github.com>
2024-08-13 19:35:54 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        'hulu-green': '#1CE783'
+      }
+    },
+  },
+  plugins: [],
+}
+
